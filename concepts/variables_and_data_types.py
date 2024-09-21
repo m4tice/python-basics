b6ec04b72f5d8d44c79a0e1bb26f630ca119a768
m4tice <ductuan2908@gmail.com>
2024-09-21 15:43:26 +0700

    Complete Variables and Data Types

---
+++ concepts/variables_and_data_types.py
@@ -0,0 +1,21 @@
+"""
+Concepts: Variables and Data Types
+Level: Beginner
+"""
+
+# Python has several native data types that are used to store data. These include:
+integer_number: int = 1
+floating_point: float = 1.0
+string_text: str = "Hello, World!"
+boolean: bool = True
+
+print("Integer Number:", integer_number)
+print("Floating Point:", floating_point)
+print("String Text:", string_text)
+print("Boolean:", boolean)
+
+# Python also has a few more complex data types that can store multiple values. These include:
+list_of_values: list = [1, 2, 3, 4, 5]                         # mutable list
+tuple_of_values: tuple = (1, 2, 3, 4, 5)                       # immutable list
+set_of_values: set = {1, 2, 3, 4, 5}                           # unique values
+dictionary_of_values: dict = {"key1": 1, "key2": 2, "key3": 3} # key-value pairs
