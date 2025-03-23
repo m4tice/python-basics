8aba12278b0780a421c181306729d82c3b552bdd
m4tice <ductuan2908@gmail.com>
2025-03-23 19:58:02 +0700

    update: refactor excessive

--- concepts/functions.py
+++ concepts/functions.py
@@ -2,20 +2,11 @@
 Concept: Functions / Function Annotations and Type Hints
 Level: Beginner
 """
-#pylint: disable=line-too-long
-
-# Python has several built-in functions that can be used to perform specific tasks. These include:
-list_any = any([True, False, True]) # Returns True if any element in the list is True
-print(". List Any:", list_any)
-
-list_all = all([True, False, True]) # Returns True if all elements in the list are True
-print(". List All:", list_all)
-
-list_enumerate = enumerate(["apple", "banana", "cherry"]) # Returns an enumerate object
-print(". List Enumerate:", list(list_enumerate))
 
 # Python also allows you to define your own functions using the 'def' keyword.
-# You can also specify the data types of the input parameters and return value using function annotations and type hints.
+# You can also specify the data types of the input parameters
+# and return value using function annotations and type hints.
+
 def add_numbers_basic(a, b):
     """
     this is a function docstring
@@ -33,5 +24,13 @@ def add_numbers_pro(a: int, b: int) -> int:
     """
     return a + b
 
-print(". Add Numbers Basic:", add_numbers_basic(1, 2))
-print(". Add Numbers Pro  :", add_numbers_pro(1, 2))
+def subtract_numbers_basic(a: int, b: int) -> int:
+    """
+    This is a function docstring following reST style.
+
+    :param a: this is a first param: int
+    :param b: this is a second param: int
+    :returns: this is a description of what is returned: int
+    :raises keyError: raises an exception (optional)
+    """
+    return a - b
